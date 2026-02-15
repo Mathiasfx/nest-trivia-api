4bb959db2e1071a9780ebd743d95ac3b06fd87d1
Mathias Pereira <mathias.pereira@piconsulting.com.ar>
2026-02-15 20:32:49 -0300

    Fix: fixrelease

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-import { ValidationPipe, HttpExceptionFilter } from '@nestjs/common';
+import { ValidationPipe} from '@nestjs/common';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
