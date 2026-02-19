b114c1c347d80243c7d2ba71e96c957c91d1320e
Mathias Pereira <mathias.pereira@piconsulting.com.ar>
2026-02-19 17:27:43 -0300

    fix: fix import module

--- src/rooms/trivias/trivias.module.ts
+++ src/rooms/trivias/trivias.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TriviasController } from './trivias.controller';
 import { TriviasService } from './trivias.service';
 import { PrismaService } from '../../prisma.service';
+import { RoomsModule } from '../rooms.module';
 
 @Module({
+  imports: [RoomsModule],
   controllers: [TriviasController],
   providers: [TriviasService, PrismaService],
   exports: [TriviasService],
